f6e4478d60639b6d3f49ac5478aa97ed09ab563a
Jorden <jakereps@users.noreply.github.com>
2017-02-11 22:47:55 -0700

    ENH: Add a bit more

--- js/main.js
+++ js/main.js
@@ -6,4 +6,25 @@
       }, 750);
     }
   });
+
+
+  var items = ['data scientist', 'technology enthusiast', 'blogger',
+               'snowboarder', 'tutor', 'software engineer'];
+  var tagline = $('#name span');
+  (function loopTransition(delay) {
+    $.each(items, function(i, element) {
+      tagline.delay(delay).fadeOut();
+      tagline.queue(function() {
+        tagline.html(items[i]);
+        tagline.dequeue();
+      })
+      tagline.fadeIn();
+      tagline.queue(function() {
+        if (i === items.length - 1) {
+          loopTransition(delay);
+        }
+        tagline.dequeue();
+      });
+    });
+  })(5000);
 })(window.jQuery);
